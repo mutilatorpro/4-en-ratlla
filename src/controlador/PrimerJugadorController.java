e6f0fc6f91f85b09446a60cc1c24572ccbfee744
= <=>
2021-04-22 19:40:19 +0200

    Merge branch 'main' of https://github.com/mutilatorpro/4-en-ratlla
    # Conflicts:
    #	src/controlador/RecordarController.java
    #	src/vista/Codi.fxml

--- src/controlador/PrimerJugadorController.java
+++ src/controlador/PrimerJugadorController.java
@@ -80,7 +80,7 @@ public class PrimerJugadorController implements Initializable {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setHeaderText(null);
         alert.setTitle("Confirmació eixida");
-        alert.setContentText("Estàs segur que vols tancar la sessió de l'usuari: " + this.jugador1.getNickName() + "?");
+        alert.setContentText("Estàs segur que vols tancar la sessió de l'usuari " + this.jugador1.getNickName() + "?");
         Optional<ButtonType> action = alert.showAndWait();
         if (action.get() == ButtonType.OK) {
             jugador1 = null;
@@ -135,7 +135,7 @@ public class PrimerJugadorController implements Initializable {
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setHeaderText(null);
         alert.setTitle("Confirmació tancar sessió");
-        alert.setContentText("Estàs segur que vols tancar la sessió de l'usuari: " + this.jugador2.getNickName() + "?");
+        alert.setContentText("Estàs segur que vols tancar la sessió de l'usuari " + this.jugador2.getNickName() + "?");
         Optional<ButtonType> action = alert.showAndWait();
         if (action.get() == ButtonType.OK) {
             numJugadors.set(numJugadors.get() - 1);
